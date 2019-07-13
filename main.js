19acd17546f92b3e0ae953cd21bef0abcc88db71
Ongarr <jakub.kulon@hotmail.com>
2019-07-13 22:20:46 +0200

    DONE

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 console.log ('Howdy Ho!');
+
+const form = document.querySelector('.signup-form');
+const feedback = document.querySelector('.feedback');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    const username = form.username.value;
+    const usernamePattern = /^[a-zA-Z]{6,12}$/;
+    if (usernamePattern.test(username)) {
+        feedback.textContent = 'That username is valid!';
+        feedback.classList.remove("is-danger");
+        feedback.classList.add('is-primary')
+    } else {
+        feedback.textContent = 'Username must contain letter only & be between 6 & 12 letters!';
+        feedback.classList.remove("is-primary");
+        feedback.classList.add('is-danger')
+    }
+    
+});
+
+

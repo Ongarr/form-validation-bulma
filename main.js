3a61bc8c4656ddc2b4c5b27eece25d702202bfa0
Ongarr <jakub.kulon@hotmail.com>
2019-07-15 06:50:04 +0200

    Adding keyboard events for form.

--- main.js
+++ main.js
@@ -1,23 +1,47 @@
 console.log ('Howdy Ho!');
 
 const form = document.querySelector('.signup-form');
-const feedback = document.querySelector('.feedback');
+const feedbackU = document.querySelector('.feedbackUser');
+const feedbackP = document.querySelector('.feedbackPass');
+const usernamePattern = /^[a-zA-Z]{6,12}$/; 
 
 form.addEventListener('submit', e => {
     e.preventDefault();
     
     const username = form.username.value;
-    const usernamePattern = /^[a-zA-Z]{6,12}$/;
+    const password = form.password.value;
+    
+    const passwordPattern = /^[a-zA-Z]{6,}$/;
+
     if (usernamePattern.test(username)) {
-        feedback.textContent = 'That username is valid!';
-        feedback.classList.remove("is-danger");
-        feedback.classList.add('is-primary')
+        feedbackU.textContent = 'That username is valid!';
+        feedbackU.classList.remove('is-danger');
+        feedbackU.classList.add('is-primary')
     } else {
-        feedback.textContent = 'Username must contain letter only & be between 6 & 12 letters!';
-        feedback.classList.remove("is-primary");
-        feedback.classList.add('is-danger')
+        feedbackU.textContent = 'Username must contain letter only & be between 6 & 12 letters!';
+        feedbackU.classList.remove("is-primary");
+        feedbackU.classList.add('is-danger')
     }
     
+    if (passwordPattern.test(password)) {
+        feedbackP.textContent = 'That password is valid!';
+        feedbackP.classList.remove('is-danger');
+        feedbackP.classList.add('is-primary')
+    } else {
+        feedbackP.textContent = 'Password must contain altleast 6 letters!';
+        feedbackP.classList.remove('is-primary');
+        feedbackP.classList.add('is-danger')
+    }
+});
+
+//Live Feedback
+form.username.addEventListener('keyup', e => {
+    if (usernamePattern.test(e.target.value)) {
+        e.target.classList.remove('is-danger');
+        e.target.classList.add('is-success');
+    } else {
+        e.target.classList.add('is-danger');
+    }
 });
 
 
